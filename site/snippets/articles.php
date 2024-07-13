d80d17afac71655d1f09e062c03dbbbe37041cfc
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-13 11:10:58 +0200

    add slot

--- site/snippets/articles.php
+++ site/snippets/articles.php
@@ -5,6 +5,7 @@
 ?>
 <section <?= HTML::attr($attrs) ?>>
         <h2 class="info">Texte von <?= $fullname ?></h2>
+				<?= $slots->default() ?>
 				<div class="article__grid all-articles">
 				<?php $index = 1; ?>
 				<?php if($articles): ?>
