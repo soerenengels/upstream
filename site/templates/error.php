888aa7f8bb8cc91e9b29da457af31dcd0594dc16
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-13 10:53:47 +0200

    add grid containers

--- site/templates/error.php
+++ site/templates/error.php
@@ -3,5 +3,11 @@
 <article>
 	<?php snippet('components/article/header', ['class' => 'error']) ?>
 	<?php snippet('layout') ?>
-	<?php snippet('forms/search') ?>
+	<section class="layout__row">
+		<section class="layout__column" style="--span: 4">
+		</section>
+		<section class="layout__column" style="--span: 8">
+			<?php snippet('forms/search') ?>
+		</section>
+	</section>
 </article>
