fc0059a368eab74cb0bab24907dbcfc2b1685799
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-13 10:56:24 +0200

    prepare controller for filters

--- site/controllers/suche.php
+++ site/controllers/suche.php
@@ -1,18 +1,43 @@
 <?php
 
-return function ($site) {
+use Kirby\Cms\Site;
 
+return function (Site $site) {
     $searchableTemplates = ['artikel', 'artikel.newsletter', 'artikel.community', 'artikel.interview', 'glossar-begriff', 'article'];
     $query   = get('q') ?? '';
-    $results = $site->search($query, 'title|text|layout|tags');
+		$type = param('type');
+		$author = param('autor');
+		$tag = param('tag');
+		$results = $site->search($query, 'title|text|layout|tags');
+		if($type) {
+			$results = $results->filterBy('type', $type);
+		}
+		if($author) {
+			$results = $results->filterBy('autor', $author);
+		}
+		if($tag) {
+			$results = $results->filterBy('tags', $tag);
+		}
     if($results):
         $results = $results->template($searchableTemplates);
     endif;
+
+		$tags = [];
+		/* foreach ($results as $result) {
+			var_dump($result);
+		} */
+	/* var_dump($tags); */
+		/* $results->map(function(\Kirby\Cms\Page $page) {
+			$tags[] = $page->tags();
+		}); */
     $results = $results->paginate(20);
+
+
 
     return [
         'query'      => $query,
         'results'    => $results,
+				'tags' => $tags,
         'pagination' => $results->pagination()
     ];
 
